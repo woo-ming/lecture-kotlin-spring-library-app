3f0086da9cdffd01031c003aa46770f87ecc7010
woo-ming <wop0123456@naver.com>
2022-08-12 22:12:20 +0900

    계산기 구현, 테스트코드 작성해보기

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,52 @@
+package com.group.libraryapp.calculator
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+}
+
+
+class CalculatorTest {
+    fun  addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        //then
+        if(calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        //then
+        if(calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        //then
+        if(calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
+
+}
